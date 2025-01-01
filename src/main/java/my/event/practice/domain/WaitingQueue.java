1b5a27f45fcb6fdc527b6310883fca132222d6b3
hwicode <khc3796@naver.com>
2025-01-01 13:24:39 +0900

    Refactor: 대기 큐의 필드명과 poll 조건을 가독성 좋게 수정함
    - repeatCount -> repeatLimit

--- src/main/java/my/event/practice/domain/WaitingQueue.java
+++ src/main/java/my/event/practice/domain/WaitingQueue.java
@@ -5,13 +5,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class WaitingQueue {
 
-    private final int repeatCount;
+    private final int repeatLimit;
     private final int pollingSize;
     private final LinkedList<String> queue;
     private final AtomicBoolean isClose;
 
-    public WaitingQueue(int repeatCount, int pollingSize) {
-        this.repeatCount = repeatCount;
+    public WaitingQueue(int repeatLimit, int pollingSize) {
+        this.repeatLimit = repeatLimit;
         this.pollingSize = pollingSize;
         this.queue = new LinkedList<>();
         isClose = new AtomicBoolean(false);
@@ -38,12 +38,15 @@ public class WaitingQueue {
         if (isClose()) {
             throw new RuntimeException();
         }
-        if (!canPoll(repeatCount)) {
+        if (!isMaxRepeat(repeatCount) && isLowItems()) {
             return Collections.emptyList();
         }
         // 반복 횟수 한계에 도달하면, pollingCount < 큐의 크기인 경우 발생
         int itemsToPoll = Math.min(queue.size(), pollingSize);
+        return getMemberIds(itemsToPoll);
+    }
 
+    private List<String> getMemberIds(int itemsToPoll) {
         List<String> memberIds = new ArrayList<>();
         for (int i = 0; i < itemsToPoll; i++) {
             memberIds.add(queue.poll());
@@ -51,9 +54,12 @@ public class WaitingQueue {
         return memberIds;
     }
 
-    // 일정 횟수 이상 시도하면 갯수가 적어도 가져감 OR 큐의 크기가 충분히 커야함
-    private boolean canPoll(int repeatCount) {
-        return repeatCount == this.repeatCount || queue.size() >= pollingSize;
+    private boolean isLowItems() {
+        return queue.size() < pollingSize;
+    }
+
+    private boolean isMaxRepeat(int repeatCount) {
+        return repeatLimit == repeatCount;
     }
 
     public boolean close() {
@@ -62,6 +68,10 @@ public class WaitingQueue {
 
     public boolean isClose() {
         return isClose.get();
+    }
+
+    public int getRepeatLimit() {
+        return repeatLimit;
     }
 
 }
