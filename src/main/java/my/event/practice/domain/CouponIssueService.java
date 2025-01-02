a03946cdfde56ab639b84674904e17575fa698cd
hwicode <khc3796@naver.com>
2025-01-02 14:24:45 +0900

    Feature: CouponIssueService 비즈니스 서비스 생성 (쿠폰 발행 담당)

---
+++ src/main/java/my/event/practice/domain/CouponIssueService.java
@@ -0,0 +1,22 @@
+package my.event.practice.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CouponIssueService {
+
+    private final WaitingQueuePoller waitingQueuePoller;
+    private final CouponManager couponManager;
+    private final CouponRepository couponRepository;
+
+    public List<Coupon> issueCoupons() {
+        List<String> memberIds = waitingQueuePoller.poll();
+        List<Coupon> coupons = couponManager.issueCoupons(memberIds);
+        return couponRepository.saveAll(coupons);
+    }
+
+}
