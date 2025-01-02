2a5e09f09cb3f90294441a72ea429a572a33ff2d
hwicode <khc3796@naver.com>
2025-01-02 14:24:06 +0900

    Feature: EventService 비즈니스 서비스 생성 (이벤트 참석 및 대기 순서 조회를 담당)

---
+++ src/main/java/my/event/practice/domain/EventService.java
@@ -0,0 +1,22 @@
+package my.event.practice.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class EventService {
+
+    private final DuplicateChecker duplicateChecker;
+    private final WaitingQueue waitingQueue;
+
+    public int participate(String memberId) {
+        duplicateChecker.check(memberId);
+        return waitingQueue.add(memberId);
+    }
+
+    public int getOrder(String memberId) {
+        return waitingQueue.getOrder(memberId);
+    }
+
+}
